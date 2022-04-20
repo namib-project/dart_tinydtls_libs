03870bcbfdc892c84c864bd757edc2ae3c4f5e07
Jan Romann <jan.romann@uni-bremen.de>
2022-04-20 13:44:32 +0200

    fix: update and clean up flutter example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,14 @@
+// Copyright 2022 The NAMIB Project Developers. All rights reserved.
+// See the README as well as the LICENSE file for more information.
+//
+// SPDX-License-Identifier: EPL-1.0 OR BSD-3-CLAUSE
+
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
 import 'package:dart_tinydtls/dart_tinydtls.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:dart_tinydtls_libs/dart_tinydtls_libs.dart';
 
 final random = Random();
 
@@ -23,41 +24,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   int _responseCounter = 0;
 
   String? _response = "No Response";
 
   bool _sending = false;
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await DartTinydtlsLibs.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
 
   // FIXME(JKRhb): After multiple exchanges tinyDTLS encounters some errors.
   //               This example should therefore only be seen as a proof of
@@ -111,8 +82,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'
-              'Request counter: $_responseCounter\n'
+          child: Text('Request counter: $_responseCounter\n'
               'Response: $_response'),
         ),
         floatingActionButton: IconButton(
